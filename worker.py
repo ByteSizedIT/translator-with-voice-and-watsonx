823e3380aac6c228d3086e159a08e8495fa9fd4a
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2025-10-06 18:48:28 +0100

    integrate Watson speech-to-text: Remember... to replace the ... for the base_url variable with the URL for your Speech-to-Text model (for example,https://sn-watson-stt.labs.skills.network that is available for use in lab environment only)

--- worker.py
+++ worker.py
@@ -39,12 +39,6 @@ model = Model(
 )
 
 
-def speech_to_text(audio_binary):
-    return None
-
-def text_to_speech(text, voice=""):
-    return None
-
 def watsonx_process_message(user_message):
     # Set the prompt for watsonx api - using a strict translation instruction
     prompt = prompt = f"""
@@ -56,3 +50,51 @@ def watsonx_process_message(user_message):
     response_text = model.generate_text(prompt=prompt)
     print("Watsonx response: ", response_text)
     return response_text.strip()
+
+###########
+
+import requests
+
+def speech_to_text(audio_binary):
+
+    # Set up Watson Speech-to-Text HTTP Api url
+    # Remember to replace the ... for the base_url variable with the URL for your Speech-to-Text model (for example,https://sn-watson-stt.labs.skills.network that is available for use in lab environment only)
+    base_url = '...'
+    api_url = base_url+'/speech-to-text/api/v1/recognize'
+
+    # Set up parameters for our HTTP reqeust
+    params = {
+        'model': 'en-US_Multimedia',  # one key-value pair i.e. 'model': 'en-US_Multimedia' which tells Watson that we want to use the US English model for processing our speech
+    }
+
+    # Set up the body of our HTTP request - sending the audio data inside the body of our POST request
+    body = audio_binary
+
+    # Send a HTTP Post request
+    response = requests.post(api_url, params=params, data=audio_binary).json()
+    '''using the requests library to send this HTTP request passing in the URL, params, and data(body) to it and then use .json() to convert the API's response to json format which is very easy to parse and can be treated like a dictionary in Python.
+    The structure of the response is something like this:
+    {
+        "response": {
+            "results": {
+            "alternatives": {
+                "transcript": "Recognised text from your speech"
+            }
+            }
+        }
+    }
+    
+    '''
+
+    # Parse the response to get our transcribed text
+    text = 'null'
+    while bool(response.get('results')):
+        print('Speech-to-Text response:', response)
+        text = response.get('results').pop().get('alternatives').pop().get('transcript')
+        print('recognised text: ', text)
+        return text
+    
+###########
+
+def text_to_speech(text, voice=""):
+    return None
